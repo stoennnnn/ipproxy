ea5511d3006a9d0fcad71e0afb12e840e5843ab9
zhangqilei <2672575920@qq.com>
2019-03-05 00:34:59 +0800

    Initial commit

---
+++ src/main/java/com/zql/IpproxyApplication.java
@@ -0,0 +1,13 @@
+package com.zql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpproxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zql/ipproxy/IpproxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zql.ipproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IpproxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
